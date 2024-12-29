1f610e5525a7126422b682f191f8cb5323700f9b
teaongsi <154658746+teaongsi@users.noreply.github.com>
2024-12-29 18:07:51 +0545

    Changes made

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-//Step 3 - Make the styling show up.
-//Hint 1: CSS files are static files!
-//Hint 2: The header and footer are partials.
-//Hint 3: Add the CSS link in header.ejs
+app.use(express.static("public"));
 
-//Step 4 - Add a dynamic year to the footer.
-//Hint: Google to find out how to get the current year using JS.
+const date=new Date();
+const year=date.getFullYear();
+res.render("footer.ejs", {yr: year});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs");
 });
 
 app.post("/submit", (req, res) => {
