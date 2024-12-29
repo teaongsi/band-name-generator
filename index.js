fb76436e4157b3c52973a6b69c8a2017bf863b7c
teaongsi <154658746+teaongsi@users.noreply.github.com>
2024-12-29 18:31:17 +0545

    refined

--- index.js
+++ index.js
@@ -13,17 +13,10 @@ app.get("/", (req, res) => {
 });
 
 app.post("/submit", (req, res) => {
-  const rndAdj=adj[Math.floor(Math.random*adj.length)];
-  const rndNoun=noun[Math.floor(Math.random*noun.length)];
+  const rndAdj=adj[Math.floor(Math.random()*adj.length)];
+  const rndNoun=noun[Math.floor(Math.random()*noun.length)];
   res.render("index.ejs", { adjective: rndAdj, noun: rndNoun});
-  //Step 2 - Make the generate name functionality work
-  //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
-  //Then:
-  //1. You should randomly pick an adjective from the const "adj" and a noun from const "noun",
-  //scroll down to see the two arrays.
-  //2. Send the index.ejs as a response and add the adjective and noun to the res.render
-  //3. Test to make sure that the random words display in the h1 element in index.ejs
-});
+}); 
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
